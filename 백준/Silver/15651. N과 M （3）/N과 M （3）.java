3cbe30c6058d121ca69a9e5f4a7ac632d0a0fd2a
vansshoes5 <82189036+vansshoes5@users.noreply.github.com>
2023-05-09 15:54:47 +0900

    [Silver III] Title: N과 M (3), Time: 512 ms, Memory: 73188 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	private static void may(ArrayList<Integer> arr, int N, int M, StringBuilder sb) {
+		
+		if(arr.size()==M) {
+			
+			for(int j:arr) sb.append(j).append(" ");
+			sb.append("\n");
+			return;
+		}
+		
+		for(int j=1;j<=N;j++) {
+			
+			arr.add(j);
+			may(arr, N, M, sb);
+			arr.remove(arr.size()-1);
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer stk = new StringTokenizer(bf.readLine(), " ");
+		int N = Integer.parseInt(stk.nextToken());
+		int M = Integer.parseInt(stk.nextToken());
+		ArrayList<Integer> ar = new ArrayList<>();
+		
+		//int[] arr = new int[N];
+		StringBuilder sb = new StringBuilder(); 
+		
+		may(ar, N, M, sb);
+		
+		System.out.println(sb);
+
+	}
+
+}
