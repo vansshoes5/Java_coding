e42726e69d6393a2bf0bc2be909ad1f71a4b4c20
vansshoes5 <82189036+vansshoes5@users.noreply.github.com>
2023-06-08 17:15:37 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int stair[];
+	static int dp[];
+
+	
+	private static int search(int N) {
+		
+		for(int i=4;i<=N;i++) {
+			dp[i] = Math.max(dp[i-2], dp[i-3]+stair[i-1])+stair[i];
+		}
+		
+		
+		return dp[N];
+	}
+	
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		//StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(bf.readLine());
+		
+		stair = new int[301];
+		dp = new int[301];
+		
+		for(int i=1;i<=N;i++) {
+			stair[i] = Integer.parseInt(bf.readLine());
+		}
+		int result = 0;
+		dp[1] = stair[1];
+		dp[2] = stair[1]+stair[2];
+		dp[3] = Math.max(stair[1], stair[2])+stair[3];
+		if(N>3) {
+			result = search(N);
+		}else {
+			result = dp[N];
+		}
+		
+		System.out.println(result);
+		
+		
+	}
+
+}
