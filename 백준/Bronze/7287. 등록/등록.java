33099947c1b12812cf13a3a1c34b65182d59c190
vansshoes5 <82189036+vansshoes5@users.noreply.github.com>
2023-08-09 11:03:50 +0900

    [Bronze V] Title: 등록, Time: 112 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+
+
+public class Main {
+	
+
+	
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub  
+		
+		//BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("237");
+		System.out.println("gompyo0219");
+		
+		
+	}
+
+}
