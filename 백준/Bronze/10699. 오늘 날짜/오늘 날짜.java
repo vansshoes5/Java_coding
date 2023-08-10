cb554acddcd9baec6413b07897555b723c337819
vansshoes5 <82189036+vansshoes5@users.noreply.github.com>
2023-08-10 17:23:41 +0900

    [Bronze V] Title: 오늘 날짜, Time: 208 ms, Memory: 17020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.time.*;
+
+/*
+ * 연구소 바이러스 막기
+ * 
+ *  1. 연구소 크기과, 연구소 내부 상태 입력받기
+ *  2. 
+ * */
+
+public class Main {
+	
+	
+	
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub  
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		LocalDate seoul = LocalDate.now(ZoneId.of("Asia/Seoul"));
+		System.out.println(seoul);
+		
+	}
+
+}
